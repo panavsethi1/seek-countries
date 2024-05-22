04acf6db3ffae2c51e88180996207e7c536c989f
panavsethi1 <panav.sethi.delhi@gmail.com>
2024-05-22 12:12:41 +0530

    fix: add loader, icon sizes

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 // NPM
 import axios from 'axios';
-import { Input, Select } from 'antd';
+import { Input, Select, Spin } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
 // Constants
@@ -24,6 +24,7 @@ import './styles/countries.css';
 function Countries() {
 	const [search, setSearch] = useState<string>('');
 	const [region, setRegion] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
 
 	const setAllCountries = useSetRecoilState(CountriesState);
 	const searchCountries = useRecoilValue(
@@ -44,12 +45,18 @@ function Countries() {
 
 	// To minimize network calls, one call is made to fetch all countries and searching is handled on the frontend
 	useEffect(() => {
+		setLoading(true);
 		axios
 			.get(BASE_URL + FIELDS_PARAM)
 			.then((res) => {
 				setAllCountries(res.data);
+				setLoading(false);
 			})
-			.catch(() => alert('Error while fetching data, please try again later.'));
+			.then(() => {})
+			.catch(() => {
+				setLoading(false);
+				alert('Error while fetching data, please try again later.');
+			});
 	}, [setAllCountries]);
 
 	return (
@@ -77,10 +84,17 @@ function Countries() {
 						))}
 					</Select>
 				</div>
-				<div className='countries-list'>
+				<div
+					style={{ alignItems: loading ? 'center' : 'initial' }}
+					className='countries-list'>
 					{searchCountries.map((country, index) => {
 						return <CountryCard key={index} country={country} />;
 					})}
+					{loading && (
+						<div className='countries-list-loader'>
+							<Spin size='large' />
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
